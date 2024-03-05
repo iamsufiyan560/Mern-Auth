8cf948480f4d88fcc7587a4ca14d79acd21f9f2f
iamsufiyan560 <140635114+iamsufiyan560@users.noreply.github.com>
2024-03-06 00:01:11 +0530

    connect to db

--- api/index.js
+++ api/index.js
@@ -1,7 +1,17 @@
-import { log } from "console";
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("connected to mongodb");
+  })
+  .catch((err) => {
+    console.log(`error ${err}`);
+  });
 
 app.listen(3000, () => {
   console.log("server runnnig on port 3000");
