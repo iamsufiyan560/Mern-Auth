d48eb8776bed1ea83164ce292b8834ef6a0bfad7
iamsufiyan560 <140635114+iamsufiyan560@users.noreply.github.com>
2024-03-07 15:44:40 +0530

    create sign api route

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -1,24 +1,22 @@
 import mongoose from "mongoose";
 
-const useSchema = new mongoose.Schema(
-  {
-    username: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-    password: {
-      type: String,
-      required: true,
-    },
+const useSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
   },
-  { timestamps: "true" }
-);
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  time: { type: Date, default: Date.now },
+});
 
 const User = mongoose.model("User", useSchema);
 export default User;
